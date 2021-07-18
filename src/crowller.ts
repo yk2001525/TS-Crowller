1bc32671568c5ce72555e0c21bd66e2c9184d3a3
”yk2001525“ <1272689242@qq.com>
2021-07-18 17:25:16 +0800

    first-upload

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,16 +1,71 @@
 //ts -> .d.ts 翻译文件 -> js
 import superagent from 'superagent';
+import cherrio from 'cheerio'
+import fs from 'fs';
+import path from 'path'
+import { data } from 'cheerio/lib/api/attributes';
+interface hot{
+    data:string;
+}
+
+interface CourseResult {
+    time:number,
+    data:hot[]
+}
+
+interface Content {
+    [propName:number]:hot
+}
 class Crowller {
     private secret = 'secretKey';
-    private url = 'https://space.bilibili.com/19121704';
+    private url = 'https://s.weibo.com/top/summary';
+    // private rawHtml = '';
 
-    private rawHtml = '';
+    getCourseInfo(html:string){
+        const $ = cherrio.load(html);
+        const td = $('.td-02');
+        const tds:hot[] = []
+        // console.log(td)
+        td.map((index,element)=>{
+            const box = $(element).find('a').text();
+            // console.log(box);
+            tds.push({data:box})
+
+        })
+        return {
+            time:(new Date()).getTime(),
+            data: tds
+        }
+
+    }
+
     async getRawHtml(){
         const result = await superagent.get(this.url)
-        console.log(result.text)
+        // console.log(result.text)
+        return result.text
+
+
+    }
+    generateJsonContent(courseInfo:CourseResult){
+        const filePath = path.resolve(__dirname,'../data/title.json');
+        let fileContent = {};
+        if(fs.existsSync(filePath)){
+            fileContent = JSON.parse(fs.readFileSync(filePath,'utf-8'));
+        }
+        console.log(courseInfo.data)
+        fileContent = courseInfo.data;
+        
+        fs.writeFileSync(filePath,JSON.stringify(fileContent))
+        // console.log(fileContent)
+    }
+    async initSpiderProcess(){
+        const html = await this.getRawHtml();
+        const courseInfo = this.getCourseInfo(html)
+        this.generateJsonContent(courseInfo);
+        console.log(courseInfo)
     }
     constructor(){
-        this.getRawHtml();
+        this.initSpiderProcess();
 
     }
 }
